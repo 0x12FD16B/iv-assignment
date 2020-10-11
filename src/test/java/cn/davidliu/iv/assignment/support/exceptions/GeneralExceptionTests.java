b833bd4f1b02ed2d6b1a5439a0a615f47d0c34c2
David Liu <recalleverything@163.com>
2020-10-11 15:44:35 +0800

    add exception test

---
+++ src/test/java/cn/davidliu/iv/assignment/support/exceptions/GeneralExceptionTests.java
@@ -0,0 +1,47 @@
+package cn.davidliu.iv.assignment.support.exceptions;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 异常测试用例
+ *
+ * @author David Liu
+ */
+public class GeneralExceptionTests {
+    private static final String EXPECTED_MESSAGE = "Test Message";
+    private static final Exception EXPECTED_CAUSE = new Exception("Nested Exception");
+
+    @Test
+    public void shouldInstantiateAndThrowAllCustomExceptions() throws Exception {
+        Class<?>[] exceptionTypes = {
+                LogConstructException.class,
+                LogInitializedException.class
+        };
+        for (Class<?> exceptionType : exceptionTypes) {
+            testExceptionConstructors(exceptionType);
+        }
+
+    }
+
+    private void testExceptionConstructors(Class<?> exceptionType) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Exception e = (Exception) exceptionType.getConstructor(String.class).newInstance(EXPECTED_MESSAGE);
+        testThrowException(e);
+        e = (Exception) exceptionType.getConstructor(String.class, Throwable.class).newInstance(EXPECTED_MESSAGE, EXPECTED_CAUSE);
+        testThrowException(e);
+        e = (Exception) exceptionType.getConstructor(Throwable.class).newInstance(EXPECTED_CAUSE);
+        testThrowException(e);
+    }
+
+    private void testThrowException(Exception thrown) {
+        try {
+            throw thrown;
+        } catch (Exception caught) {
+            assertEquals(thrown.getMessage(), caught.getMessage());
+            assertEquals(thrown.getCause(), caught.getCause());
+        }
+    }
+}
