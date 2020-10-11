275825bc9982f442e5b34e3f81b9e6956010de11
David Liu <recalleverything@163.com>
2020-10-11 14:38:27 +0800

    Add LogFactory Test Case

---
+++ src/test/java/cn/davidliu/iv/assignment/support/log/LogFactoryTest.java
@@ -0,0 +1,33 @@
+package cn.davidliu.iv.assignment.support.log;
+
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LogFactoryTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void shouldUseStdOutLogging() {
+        LogFactory.useStdOutLogging();
+        Log log = LogFactory.getLog(Object.class);
+        logSomething(log);
+        Assert.assertEquals(log.getClass().getName(), StdOutImpl.class.getName());
+    }
+
+    private void logSomething(Log log) {
+        log.warn("Warning message.");
+        log.debug("Debug message.");
+        log.error("Error message.");
+        log.error("Error with Exception.", new Exception("Test exception."));
+    }
+}
