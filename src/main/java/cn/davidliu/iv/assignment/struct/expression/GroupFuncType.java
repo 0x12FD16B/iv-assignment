623213e64ebcf44c3ba158b0e175357641a00b69
David Liu <recalleverything@163.com>
2020-10-12 21:17:48 +0800

    add group function type enum

---
+++ src/main/java/cn/davidliu/iv/assignment/struct/expression/GroupFuncType.java
@@ -0,0 +1,15 @@
+package cn.davidliu.iv.assignment.struct.expression;
+
+/**
+ * 分组函数类型
+ *
+ * @author David Liu
+ */
+public enum GroupFuncType {
+    // 求和
+    SUM,
+    // 求总数
+    COUNT,
+    // AVG
+    AVG;
+}
