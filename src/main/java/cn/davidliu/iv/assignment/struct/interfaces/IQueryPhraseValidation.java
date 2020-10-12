e4538e80af590c11cec3308ba3bb840f9946803c
David Liu <recalleverything@163.com>
2020-10-12 21:18:43 +0800

    add query phrase validation interface

---
+++ src/main/java/cn/davidliu/iv/assignment/struct/interfaces/IQueryPhraseValidation.java
@@ -0,0 +1,15 @@
+package cn.davidliu.iv.assignment.struct.interfaces;
+
+/**
+ * 查询短语校验接口
+ *
+ * @author David Liu
+ */
+public interface IQueryPhraseValidation {
+
+    /**
+     * 查询短语校验
+     */
+    void validate();
+
+}
